a11d7dd1cef0ae2d07bfc41c158702d6c42276bb
Manish Jain <manish.jain@commonfloor.com>
2015-06-13 23:41:36 +0530

    #8: User listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], :per_page => 2)
   end
 
   # GET /users/1
@@ -76,4 +78,17 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,16 @@
 module SessionsHelper
+
 	def log_in(user)
-    session[:user_id] = user.id
-  end
+		session[:user_id] = user.id
+	end
 
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def logged_in?
