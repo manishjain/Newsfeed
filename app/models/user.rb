c83058c8af0bcb7891e55bcaae6828ffaa0431d8
Manish Jain <manish.jain@commonfloor.com>
2015-06-13 20:58:51 +0530

    #6: Addying Password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, length: { minimum: 6 }
 end
