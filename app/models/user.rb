22478fd34ee9aaf24285fa836aca6dbcc0419d7f
Manish Jain <manish.jain@commonfloor.com>
2015-06-14 00:30:39 +0530

    #10: addying posts in user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	# attr_accessor :name, :email
 	before_save { self.email = email.downcase }
-	has_many :microposts, dependent: :destroy
+	has_many :posts, dependent: :destroy
 	validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   has_secure_password
