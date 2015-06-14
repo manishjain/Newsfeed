db378e8ede948692103e836906e2fd19e392c31b
Manish Jain <manish.jain@commonfloor.com>
2015-06-14 19:06:21 +0530

    #17: likes and comments for create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  resources :comments
-
-  resources :likes
-
   get 'sessions/new'
 
   get 'static_pages/home'
   get 'static_pages/help'
 
   resources :posts,          only: [:create, :destroy]
+  resources :comments, only: [:create, :destroy]
+  resources :likes, only: [:create, :destroy]
 
   resources :users
-  # root 'users#index'
+  # root 'users#index
   root 'static_pages#home'
 
   get    'signup'  => 'users#new'
