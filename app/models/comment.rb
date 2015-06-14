1cf242c13d34a89b2c24f9b3bdd2c3de28cd83b6
Manish Jain <manish.jain@commonfloor.com>
2015-06-14 18:25:24 +0530

    #14: comments validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post, -> { where(target_type: 'post') }, foreign_key: "target_id"
 	belongs_to :comment, -> { where(target_type: 'comment') }, foreign_key: "target_id"
+	validates :target_id, presence: true
+	validates :target_type, presence: true
+	validates :user_id, presence: true
+	validates :content, length: { maximum: 200 }, presence: true
 end
