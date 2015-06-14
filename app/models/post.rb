e944d60130e0cceff8ab7923b30c9f87df35bde8
Manish Jain <manish.jain@commonfloor.com>
2015-06-14 13:04:23 +0530

    #12: added polymorphic association

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ActiveRecord::Base
 	default_scope -> { order(updated_at: :desc) }
 	validates :user_id, presence: true
 	validates :content, length: { maximum: 200 }, presence: true
-	has_many :likes, -> { where(target_type: 'post') }
+	has_many :likes, -> { where(target_type: 'post') },  :foreign_key => :target_id
+	has_many :comments, -> { where(target_type: 'comment') },  :foreign_key => :target_id
 
 	def self.getFeed
 		Post.all
