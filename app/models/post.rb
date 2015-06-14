6db0ce2a01a8e68f59fdcd737cdd9e878bacb400
Manish Jain <manish.jain@commonfloor.com>
2015-06-14 11:43:31 +0530

    #12: order of feed changed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	default_scope -> { order(created_at: :desc) }
+	default_scope -> { order(updated_at: :desc) }
 	validates :user_id, presence: true
 	validates :content, length: { maximum: 200 }, presence: true
 
